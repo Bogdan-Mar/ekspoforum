01806977c547966b61e2033d0eff13976d2c11db
Bogdan <bodia.marisyuk@gmail.com>
2020-02-06 21:46:31 +0200

    first

---
+++ js/common.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    //preloader
+    $('body').addClass('loaded');
+
+    //fixed nav bar
+    var yourNavigation = $(".top_menu");
+    var stickyDiv      = "sticky";
+    var yourHeader     = $(".top_menu").height();
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > yourHeader) {
+            yourNavigation.addClass(stickyDiv);
+        } else {
+            yourNavigation.removeClass(stickyDiv);
+        }
+    });
+
+    $('.burger').on('click', function() {
+        if( $("body").hasClass("mmenu_active") ) {
+            $("body").removeClass("mmenu_active") ;
+        } else {
+            $("body").addClass("mmenu_active") ;
+        }             
+    });
+
+
+   
+
+});
+
